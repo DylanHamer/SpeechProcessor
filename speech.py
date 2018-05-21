6ba0d6b2ed306d1ab5460be9d707517fd810c2d3
Dylan Hamer <dylanhamer13@gmail.com>
2018-05-21 12:47:11 +0100

    Apparently the Google Cloud platform costs money

--- speech.py
+++ speech.py
@@ -1,4 +1,11 @@
+"""
+VoiceProcessor
+Wrapper for Google Speech API - Apparently the Google Cloud platform costs money so I have no idea if this works or not
+By Dylan Hamer
+"""
+
 import requests
+import base64
 
 apiURL = "https://speech.googleapis.com/v1/speech:recognize"
 
@@ -14,11 +21,9 @@ speechJSON = {
   }
 }
 
-def hypothosis():
-    with open("speechFile.mp3") as speechFile:
-        speechJSON["audio"]["content"] = speechFile.open()
-        json = requests.post(apiURL, data=speechJSON).json
-    return json["hypothosis"]
-
-print(hypothosis())
-    
+def hypothesis():
+    with open("speech.mp3", "rb") as speechFile:
+        encodedFile = base64.b64encode(speechFile.read())
+        speechJSON["audio"]["content"] = encodedFile
+        json = requests.post(apiURL, data=speechJSON).json()
+    return json
